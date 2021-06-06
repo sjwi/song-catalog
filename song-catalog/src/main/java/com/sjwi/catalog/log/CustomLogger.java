99e7d5d90a3e5ccbeb1302cbc0b02db0d4c95567
Stephen Williams <stephenjw@fastmail.com>
2021-06-06 11:22:41 -0400

    Remove redundant "properName"

--- song-catalog/src/main/java/com/sjwi/catalog/log/CustomLogger.java
+++ song-catalog/src/main/java/com/sjwi/catalog/log/CustomLogger.java
@@ -10,7 +10,6 @@ import com.sjwi.catalog.model.mail.Email;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -24,9 +23,6 @@ public class CustomLogger {
 
 	@Autowired
 	Mailer mailer;
-
-	@Value("${com.sjwi.settings.app.properName}")
-	String properName;
 
 	private Logger log;
 
