011135d9743d7a6ab593dc72e7a864e02325da7a
Stephen Williams <stephenjw@fastmail.com>
2021-06-20 17:09:44 -0400

    Hard code mail strings outside of init

--- song-catalog/src/main/java/com/sjwi/catalog/mail/MailConstants.java
+++ song-catalog/src/main/java/com/sjwi/catalog/mail/MailConstants.java
@@ -4,21 +4,17 @@ public class MailConstants {
 
 	public static String DEFAULT_FROM_ADDRESS;
 	public static String ADMIN_DISTRIBUTION_LIST;
-	public static String LOG_NOTIFICATION_SUBJECT;
-	public static String ERROR_SUBJECT;
 	public static String INVITATION_SUBJECT;
 	public static String PASSWORD_RESET_SUBJECT;
-	public static String EDIT_ACTION;
-	public static String VERSION_CREATED_ACTION;
+	public static String LOG_NOTIFICATION_SUBJECT = "Log Notification";
+	public static String ERROR_SUBJECT = "ERROR";
+	public static String EDIT_ACTION = "Song edited";
+	public static String VERSION_CREATED_ACTION = "New version created";
 	
 	public static void initializeMailConstants(String defaultFromAddress, String adminDistributionList, String properName) {
 		DEFAULT_FROM_ADDRESS = defaultFromAddress;
 		ADMIN_DISTRIBUTION_LIST = adminDistributionList;
-		LOG_NOTIFICATION_SUBJECT = "Log Notification";
-		ERROR_SUBJECT = "ERROR";
 		INVITATION_SUBJECT = "{{USERNAME}} has invited you to join the " + properName + " Website";
 		PASSWORD_RESET_SUBJECT = "Password Reset Requested - " + properName;
-		EDIT_ACTION = "Song edited";
-		VERSION_CREATED_ACTION = "New version created";
 	}
 }
