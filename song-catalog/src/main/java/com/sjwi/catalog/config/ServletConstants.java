cf9b16d80a8e1be34d9d5519a9082f17f97b742e
Stephen Williams <stephenjw@fastmail.com>
2021-07-15 22:16:55 -0400

    Initialize servlet constants from request

--- song-catalog/src/main/java/com/sjwi/catalog/config/ServletConstants.java
+++ song-catalog/src/main/java/com/sjwi/catalog/config/ServletConstants.java
@@ -1,28 +1,14 @@
 package com.sjwi.catalog.config;
 
-import java.lang.management.ManagementFactory;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
-import javax.annotation.PostConstruct;
-import javax.management.AttributeNotFoundException;
-import javax.management.InstanceNotFoundException;
-import javax.management.MBeanException;
-import javax.management.MBeanServer;
-import javax.management.MalformedObjectNameException;
-import javax.management.ObjectName;
-import javax.management.Query;
-import javax.management.QueryExp;
-import javax.management.ReflectionException;
-import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
 public class ServletConstants {
@@ -34,47 +20,17 @@ public class ServletConstants {
     public static String BASE_URL;
     public static String FULL_URL;
     
-    @Autowired
-    public ServletContext context;
-
-    @Value("${server.port}")
-    private int containerPort;
-
-    private static final String DEPLOYMENT_SERVER = "stephenky.com";
-
     private static final List<String> IGNORE_PORT_LIST = new ArrayList<String>(Arrays.asList("80","443","8080","8443"));
 
-    @PostConstruct
-    public void initializeServletConstants() throws MalformedObjectNameException,
-            NullPointerException, UnknownHostException, AttributeNotFoundException,
-            InstanceNotFoundException, MBeanException, ReflectionException {
-        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-        QueryExp subQuery1 = Query.match(Query.attr("protocol"), Query.value("HTTP/1.1"));
-        QueryExp subQuery2 = Query.anySubString(Query.attr("protocol"), Query.value("Http11"));
-        QueryExp query = Query.or(subQuery1, subQuery2);
-        Set<ObjectName> objs = mbs.queryNames(new ObjectName("*:type=Connector,*"), query);
-        String hostname = InetAddress.getLocalHost().getHostName();
-        String scheme = "http";
-        String port = String.valueOf(containerPort);
-        String host = "localhost";
-        if (objs.iterator().hasNext()) {
-            ObjectName obj = objs.iterator().next();
-            scheme = mbs.getAttribute(obj, "scheme").toString();
-            port = obj.getKeyProperty("port");
-            host = InetAddress.getAllByName(hostname)[0].getHostAddress();
-        } 
-        if ("192.168.1.45".equals(host))
-            SERVER_NAME = "localhost";
-        else if ("127.0.1.1".equals(host))
-            SERVER_NAME = DEPLOYMENT_SERVER;
-        else
-            SERVER_NAME = host;
-        SCHEME = host.contains(".com")? "https" : scheme;
-        SERVER_PORT = port;
-        CONTEXT_PATH = context.getContextPath().toString().replaceAll("/","");
-        BASE_URL = scheme + "://" + SERVER_NAME;
-        if (!IGNORE_PORT_LIST.contains(port))
-            BASE_URL += ":" + port;
+    public static void initializeServletConstants() {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        SERVER_NAME = request.getServerName();
+        SCHEME = SERVER_NAME.contains(".com")? "https" : request.getScheme();
+        SERVER_PORT = String.valueOf(request.getServerPort());
+        CONTEXT_PATH = request.getContextPath().replaceAll("/","");
+        BASE_URL = SCHEME + "://" + SERVER_NAME;
+        if (!IGNORE_PORT_LIST.contains(SERVER_PORT))
+            BASE_URL += ":" + SERVER_PORT;
         FULL_URL = CONTEXT_PATH.trim().isEmpty()? BASE_URL: BASE_URL + "/" + CONTEXT_PATH;
     }
 }
