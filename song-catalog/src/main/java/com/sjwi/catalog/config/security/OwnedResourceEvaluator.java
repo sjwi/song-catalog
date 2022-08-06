b6cc7b2eba5edafa2d404d7889dd4041d17e211c
Stephen Williams <stephenjw@fastmail.com>
2022-08-06 16:03:43 -0400

    Unauthorized exception handler

--- song-catalog/src/main/java/com/sjwi/catalog/config/security/OwnedResourceEvaluator.java
+++ song-catalog/src/main/java/com/sjwi/catalog/config/security/OwnedResourceEvaluator.java
@@ -14,13 +14,14 @@ import com.sjwi.catalog.model.user.UserRoles;
 public class OwnedResourceEvaluator implements BiConsumer<OwnedResource, Principal> {
   @Override
   public void accept(OwnedResource t, Principal p) {
+    UsernamePasswordAuthenticationToken u = null;
     try {
-      UsernamePasswordAuthenticationToken u = (UsernamePasswordAuthenticationToken) p;
-      Boolean userIsAdmin = u.getAuthorities().stream().map(a -> a.getAuthority()).anyMatch(a -> a.equals(UserRoles.ADMIN.name()));
-      if (!userIsAdmin && !u.getName().equals(t.getOwner()))
-        throw new UnauthorizedException("User does not own this resource and does not have permission to change it.");
+      u = (UsernamePasswordAuthenticationToken) p;
     } catch (Exception e) {
         throw new UnauthorizedException("Invalid token");
     }
+    Boolean userIsAdmin = u.getAuthorities().stream().map(a -> a.getAuthority()).anyMatch(a -> a.equals(UserRoles.ADMIN.name()));
+    if (!userIsAdmin && !u.getName().equals(t.getOwner()))
+      throw new UnauthorizedException("User does not own this resource and does not have permission to change it.");
   }
 }
--- song-catalog/src/main/java/com/sjwi/catalog/controller/ExceptionHandler.java
+++ song-catalog/src/main/java/com/sjwi/catalog/controller/ExceptionHandler.java
@@ -1,15 +1,19 @@
 /* (C)2022 https://stephenky.com */
 package com.sjwi.catalog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.sjwi.catalog.exception.UnauthorizedException;
 import com.sjwi.catalog.log.CustomLogger;
 import com.sjwi.catalog.model.ExceptionResponse;
 import com.sjwi.catalog.model.ExceptionResponse.ExceptionResponseCode;
 import com.sjwi.catalog.service.OrganizationService;
+
 import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 @Log
@@ -27,5 +31,10 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
         .body(
             new ExceptionResponse(
                 ExceptionResponseCode.INTERNAL_SERVER_ERROR, "Unable to process your request"));
+  }
+
+  @org.springframework.web.bind.annotation.ExceptionHandler({UnauthorizedException.class})
+  public ResponseEntity<ExceptionResponse> unauthorizedExceptionHandler(UnauthorizedException e) {
+    return new ResponseEntity<>(new ExceptionResponse(ExceptionResponseCode.UNAUTHORIZED, e.getMessage()), HttpStatus.UNAUTHORIZED);
   }
 }
