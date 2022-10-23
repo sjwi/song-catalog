7af3c5f3ecb4b2ed86e4094121b498982a377757
Stephen Williams <stephenjw@fastmail.com>
2022-10-23 16:36:19 -0400

    call cached function

--- song-catalog/src/main/resources/static/js/site/scroll.js
+++ song-catalog/src/main/resources/static/js/site/scroll.js
@@ -22,12 +22,12 @@ function addScrollListener(id, size, delay){
 			if ((prevScrollpos > currentScrollPos && velocity > velocityThreshold) || $(window).scrollTop() <= delay) {
 				$(id).css('top','0');
 				$('.sticky-top-nav, .top-nav').css('top','0');
-				if (slideAlertUp && $('.add-songs-alert').hasClass('slide-down') && checkedCacheSize == 0)
+				if (slideAlertUp && $('.add-songs-alert').hasClass('slide-down') && checkedCacheSize() != 0)
 					slideAlertUp()
 			} else if (velocity > velocityThreshold) {
 				$(id).css('top',size);
 				$('.sticky-top-nav, .top-nav').css('top',size);
-				if (slideAlertDown && !$('.add-songs-alert').hasClass('slide-down'))
+				if (slideAlertDown && !$('.add-songs-alert').hasClass('slide-down') && checkedCacheSize() != 0)
 					slideAlertDown()
 			}
 			prevScrollpos = currentScrollPos;	
