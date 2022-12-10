32cf8cb55c778c7ad3601dd10ec6886fb3f37b38
Stephen Williams <stephenjw@fastmail.com>
2022-12-10 12:10:07 -0500

    better shorlink key

--- song-catalog/src/main/java/com/sjwi/catalog/task/DbCleanup.java
+++ song-catalog/src/main/java/com/sjwi/catalog/task/DbCleanup.java
@@ -2,7 +2,9 @@
 package com.sjwi.catalog.task;
 
 import com.sjwi.catalog.dao.UserDao;
+import com.sjwi.catalog.log.CustomLogger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +12,15 @@ import org.springframework.stereotype.Component;
 public class DbCleanup {
 
   @Autowired UserDao userDao;
+  @Autowired CustomLogger logger;
 
   @Scheduled(cron = "0 0 * * * *")
+  @ConditionalOnProperty(
+      value = "spring.datasource.driver-class-name",
+      havingValue = "com.mysql.jdbc.Driver",
+      matchIfMissing = false)
   public void cleanDb() {
+    logger.logMessageWithEmail("Cleaning up bots from db");
     userDao.cleanBots();
   }
 }
