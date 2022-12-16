f800cf95a96a3a78293cdbf999ad0413445716b6
Stephen Williams <stephenjw@fastmail.com>
2022-12-16 12:25:55 -0500

    remove thymeleaf cache

--- song-catalog/src/main/java/com/sjwi/catalog/controller/FileDownloadController.java
+++ song-catalog/src/main/java/com/sjwi/catalog/controller/FileDownloadController.java
@@ -363,16 +363,11 @@ public class FileDownloadController {
   }
 
   @GetMapping(value = {"{prefix}z{suffix}"})
-  public ModelAndView shortLink(@PathVariable String prefix, @PathVariable String suffix) {
-    try {
-      String key = String.format("%sz%s", prefix, suffix);
-      String path = shortLinkService.getPath(key);
-      String action = controllerHelper.getOs().equalsIgnoreCase("UNKNOWN") ? "redirect" : "forward";
-      logger.logUserActionWithEmail(String.format("Short link %s visited", key));
-      return new ModelAndView(action + ":/" + path);
-    } catch (Exception e) {
-      e.printStackTrace();
-      throw e;
-    }
+  public ModelAndView shortLink(@PathVariable String prefix, @PathVariable String suffix, HttpServletRequest request) {
+    String key = String.format("%sz%s", prefix, suffix);
+    String path = shortLinkService.getPath(key);
+    String action = controllerHelper.getOs().equalsIgnoreCase("UNKNOWN") ? "redirect" : "forward";
+    logger.logUserActionWithEmail(String.format("Short link %s visited", key));
+    return new ModelAndView(action + ":/" + path);
   }
 }
