8e8a275e863ef6a6468aacdae3e6409a995ae949
Stephen Williams <swilliams@appomni.com>
2023-10-18 13:50:01 -0400

    10 min not 10 secs

--- song-catalog/src/main/java/com/sjwi/catalog/task/CacheMgmt.java
+++ song-catalog/src/main/java/com/sjwi/catalog/task/CacheMgmt.java
@@ -1,11 +1,12 @@
 /* (C)2022 https://stephenky.com */
 package com.sjwi.catalog.task;
 
-import com.sjwi.catalog.service.SetListService;
-import com.sjwi.catalog.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.sjwi.catalog.service.SetListService;
+import com.sjwi.catalog.service.SongService;
 
 @Component
 public class CacheMgmt {
@@ -13,8 +14,9 @@ public class CacheMgmt {
   @Autowired SongService songService;
   @Autowired SetListService setListService;
 
-  @Scheduled(cron = "*/10 * * * * *")
+  @Scheduled(cron = "0 */10 * * * *")
   public void cleanDb() {
+    System.out.println("REFRESHING");
     songService.refreshSongCache();
     setListService.refreshSetListCache();
   }
