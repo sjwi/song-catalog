c910e24a390240c420f54828aeac047b42322803
Stephen Williams <swilliams@appomni.com>
2023-10-18 13:51:44 -0400

    rm print

--- song-catalog/src/main/java/com/sjwi/catalog/task/CacheMgmt.java
+++ song-catalog/src/main/java/com/sjwi/catalog/task/CacheMgmt.java
@@ -1,12 +1,11 @@
 /* (C)2022 https://stephenky.com */
 package com.sjwi.catalog.task;
 
+import com.sjwi.catalog.service.SetListService;
+import com.sjwi.catalog.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import com.sjwi.catalog.service.SetListService;
-import com.sjwi.catalog.service.SongService;
 
 @Component
 public class CacheMgmt {
@@ -16,7 +15,6 @@ public class CacheMgmt {
 
   @Scheduled(cron = "0 */10 * * * *")
   public void cleanDb() {
-    System.out.println("REFRESHING");
     songService.refreshSongCache();
     setListService.refreshSetListCache();
   }
